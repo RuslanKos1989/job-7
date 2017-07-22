b246681739a7d42e07380716cf06df40fcdfd2f2
Ruslan Minnibaev <arrusl1989@gmail.com>
2017-07-22 17:35:30 +0300

    Основная верстка окончена

--- app/js/common.js
+++ app/js/common.js
@@ -130,4 +130,12 @@ $(".carousel-control-next").on("click", function(){
 		// console.log(current);
 	});
 
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.9";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 });
